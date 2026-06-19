7c077d6d27807b526dcce8fe287f951095fcfbf5
manu <manugarcezconde@gmail.com>
2026-06-19 20:14:39 -0300

    Add menu navigation for memory game

---
+++ jogos/jogo-da-memoria/menuMemoria.js
@@ -0,0 +1,11 @@
+DocumentType.getElementbyId("b1"),addEventListener("click", function (){
+    window.location.href = "jm1p.html";
+})
+
+DocumentType.getElementbyId("b2"),addEventListener("click", function (){
+    window.location.href = "jm2p.html";
+})
+
+DocumentType.getElementbyId("s"),addEventListener("click", function (){
+    window.location.href = "COLOCAR LINK AQUI";
+})
