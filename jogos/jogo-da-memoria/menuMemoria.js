60b266394c277e588a112eb1d681b049ed30997d
manu <manugarcezconde@gmail.com>
2026-06-23 21:25:34 -0300

    Remove button 'b2' event listener
    Removed event listener for button 'b2' that redirected to 'jm2p.html'.

--- jogos/jogo-da-memoria/menuMemoria.js
+++ jogos/jogo-da-memoria/menuMemoria.js
@@ -2,10 +2,6 @@ DocumentType.getElementbyId("b1"),addEventListener("click", function (){
     window.location.href = "jm1p.html";
 })
 
-DocumentType.getElementbyId("b2"),addEventListener("click", function (){
-    window.location.href = "jm2p.html";
-})
-
 DocumentType.getElementbyId("s"),addEventListener("click", function (){
     window.location.href = "index.html";
 })
